4ad781f2d3f5c47626a2c49b31e255ab41e1c5ba
Bengisu Bostancı <bengisubostanci0@gmail.com>
2026-05-26 15:12:51 +0300

    Create 1-main.go

---
+++ 1-main.go
@@ -0,0 +1,22 @@
+ "Hello, World!"
+
+Go
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Merhaba, Go dünyası!")
+}
+
+
+Variables and Data Types
+
+var isim string = "Bengisu"
+var yas int = 24
+var saniyedeVeri float64 = 1500.50
+var veriMi boolean = true
+
+sehir := "Ankara" // Go bunun bir 'string' olduğunu anlar
+yil := 2026       // Go bunun bir 'int' olduğunu anlar
+
